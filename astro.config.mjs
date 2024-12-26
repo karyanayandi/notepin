e7fa214ad0aa1de167dc5636f0c6e4684f4c2a62
karyanayandi <xkaryanayandi@gmail.com>
2024-12-26 19:07:24 +0700

    fix: update vercel import to use base package
    The import statement is simplified to use the base @astrojs/vercel package
    instead of specifically importing from the serverless entrypoint.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel"
 
 export default defineConfig({
   integrations: [tailwind(), svelte()],
